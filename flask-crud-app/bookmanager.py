facd311c7b186fc61aa70d20a67887e67a9295e6
kcodek <career_kk@yahoo.com>
2018-07-04 12:18:32 +0530

    finishing touches

--- flask-crud-app/bookmanager.py
+++ flask-crud-app/bookmanager.py
@@ -21,22 +21,31 @@ class Book(db.Model):
 
 @app.route('/',methods=["GET","POST"])
 def home():
+    books = None
     if request.form:
-        # print(request.form)
-        book = Book(title=request.form.get('title'))
-        db.session.add(book)
-        db.session.commit()
+        try:
+            book = Book(title=request.form.get("title"))
+            db.session.add(book)
+            db.session.commit()
+        except Exception as e:
+            print("Failed to add book")
+            print(e)
+            return redirect("/")
     books = Book.query.all()
-    return render_template("home.html",books=books)
+    return render_template("home.html", books=books)
 
-@app.route("/update",methods=["POST"])
+@app.route("/update", methods=["POST"])
 def update():
-    newtitle = request.form.get("newtitle")
-    oldtitle = request.form.get("oldtitle")
-    book = Book.query.filter_by(title=oldtitle).first()
-    book.title = newtitle
-    db.session.commit()
-    return redirect('/')
+    try:
+        newtitle = request.form.get("newtitle")
+        oldtitle = request.form.get("oldtitle")
+        book = Book.query.filter_by(title=oldtitle).first()
+        book.title = newtitle
+        db.session.commit()
+    except Exception as e:
+        print("Couldn't update book title")
+        print(e)
+    return redirect("/")
 
 @app.route("/delete", methods=["POST"])
 def delete():
