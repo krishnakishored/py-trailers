ae0bd2c932553d703e8ffe0fa3b4596427ba0721
kishore.donthikurthi <kishore.donthikurthi@ispace.com>
2018-07-04 12:02:48 +0530

    Update and Delete

--- flask-crud-app/bookmanager.py
+++ flask-crud-app/bookmanager.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -28,6 +28,23 @@ def home():
         db.session.commit()
     books = Book.query.all()
     return render_template("home.html",books=books)
+
+@app.route("/update",methods=["POST"])
+def update():
+    newtitle = request.form.get("newtitle")
+    oldtitle = request.form.get("oldtitle")
+    book = Book.query.filter_by(title=oldtitle).first()
+    book.title = newtitle
+    db.session.commit()
+    return redirect('/')
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    title = request.form.get("title")
+    book = Book.query.filter_by(title=title).first()
+    db.session.delete(book)
+    db.session.commit()
+    return redirect("/")
 
 if __name__=="__main__":
     app.run(debug=True)
